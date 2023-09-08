db180e1b75cb632939cf312dabf170a8463af90e
Yugo <satoyugo1022@gmail.com>
2023-09-09 01:18:51 +0900

    first commit

---
+++ app/Http/Controllers/Auth/UserRegisterController.php
@@ -0,0 +1,26 @@
+<?php
+
+// namespace App\Http\Controllers\Auth;
+
+// use App\Http\Controllers\Controller;
+// use Illuminate\Http\Request;
+// use App\Http\Requests\RegisterRequest;
+// use App\Models\User;
+
+// class UserRegisterController extends Controller
+// {
+//     /**
+//      * Handle the incoming request.
+//      */
+//     public function __invoke(Request $request)
+//     {
+//         $user = User::create($request->getData());
+//         $token = $user->createToken('user_token')->plainTextToken;
+
+//         return response()->json([
+//             'data' => $user,
+//             'token' => $token,
+//             'message' => 'User registered successfully!',
+//         ]);
+//     }
+// }
