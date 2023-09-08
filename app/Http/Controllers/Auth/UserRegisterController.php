b0b66fd23a797457fc54c66d795b29c0214807b7
Yugo <satoyugo1022@gmail.com>
2023-09-09 02:16:52 +0900

    add register func

--- app/Http/Controllers/Auth/UserRegisterController.php
+++ app/Http/Controllers/Auth/UserRegisterController.php
@@ -1,26 +1,24 @@
 <?php
 
-// namespace App\Http\Controllers\Auth;
+namespace App\Http\Controllers\Auth;
 
-// use App\Http\Controllers\Controller;
-// use Illuminate\Http\Request;
-// use App\Http\Requests\RegisterRequest;
-// use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 
-// class UserRegisterController extends Controller
-// {
-//     /**
-//      * Handle the incoming request.
-//      */
-//     public function __invoke(Request $request)
-//     {
-//         $user = User::create($request->getData());
-//         $token = $user->createToken('user_token')->plainTextToken;
+class UserRegisterController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(RegisterRequest $request)
+    {
+        $user = User::create($request->getData());
 
-//         return response()->json([
-//             'data' => $user,
-//             'token' => $token,
-//             'message' => 'User registered successfully!',
-//         ]);
-//     }
-// }
+        return response()->json([
+            'data' => $user,
+            'message' => 'Registration successful',
+        ]);
+    }
+}
